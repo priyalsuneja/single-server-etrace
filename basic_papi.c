25c28aad56feb343b7b4566fdbe918e2e52ae8a9
Priyal Suneja <suneja@zookeeper.cs.washington.edu>
2021-11-09 19:41:45 -0800

    basic papi code measuring two events

--- basic_papi.c
+++ basic_papi.c
@@ -23,24 +23,31 @@ int main (int main, char **argv) {
         fprintf(stderr, "papi add eventset %s\n", PAPI_strerror(retval));
         return 0;
     }
+    retval = PAPI_add_event(eventset, PAPI_TOT_CYC);
+    if(retval != PAPI_OK) {
+        fprintf(stderr, "papi add eventset cyc %s\n", PAPI_strerror(retval));
+        return 0;
+    }
     
-    long long count; 
+    long long count[2]; 
 
     PAPI_reset(eventset);
     retval = PAPI_start(eventset);
-    if(retval != PAPI_OK) { 
-        fprintf(stderr, "error starting CUDA: %s\n", PAPI_strerror(retval));
-    } 
+//     if(retval != PAPI_OK) { 
+//         fprintf(stderr, "error starting CUDA: %s\n", PAPI_strerror(retval));
+//     } 
     printf("hello world\n");
+//     int x = 55;
+    printf("bye bye\n");
     
-    retval=PAPI_stop(eventset, &count);
+    retval=PAPI_stop(eventset, count);
     if(retval!=PAPI_OK) {
         fprintf(stderr, "papi error stopping %s\n", PAPI_strerror(retval));
         return 0;
         
     }
     else {
-        printf("Measured %lld cycles\n", count);
+        printf("Measured %lld ins, %lld cycles\n", count[0], count[1]);
     }
 
     PAPI_cleanup_eventset(eventset);
